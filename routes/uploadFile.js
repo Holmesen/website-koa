78a502b0efad2e21ac8048da620faa291753086b
linss <1315654996@qq.com>
2019-11-26 18:17:22 +0800

    添加博客图片上传api

--- routes/uploadFile.js
+++ routes/uploadFile.js
@@ -2,13 +2,24 @@ const Router = require('koa-router')
 const router = new Router()
 const upload = require('../lib/uploadFile')
 
-router.post('/avatar', upload.single('file'), async(ctx, next)=>{
+router.post('/avatar', upload('avatar').single('file'), async(ctx, next)=>{
   ctx.body = {
     success: true,
     msg: '头像上传成功！',
     data: { 
       name: ctx.req.file.filename,
       path: `/images/avatar/${ctx.req.file.filename}`
+    }
+  }
+})
+
+router.post('/blog-image', upload('blog').single('file'), async(ctx, next)=>{
+  ctx.body = {
+    success: true,
+    msg: '博客图片上传成功！',
+    data: { 
+      name: ctx.req.file.filename,
+      path: `/images/blog/${ctx.req.file.filename}`
     }
   }
 })
